17076d31af94fb1dd00eaee94b0bdfb111bcd493
James <jamestariga08@live.com>
2022-05-16 15:37:35 -0700

    ✨ First Commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = process.env.PORT || 3500
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
