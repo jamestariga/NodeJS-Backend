742d2a6b538337e8d08a9cf9de1e7ed6b0d91b8f
James <jamestariga08@live.com>
2022-05-19 12:33:00 -0700

    ✨ Minor change and Nodemon installed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path')
 const PORT = process.env.PORT || 3500
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'))
+  res.sendFile(path.join(__dirname, 'Views', 'index.html'))
 })
 
 app.listen(PORT, () => {
