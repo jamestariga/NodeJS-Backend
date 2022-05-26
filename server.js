f43e33f90ee333d9c8c873bc2e91ec8bc6e6c417
James <jamestariga08@live.com>
2022-05-25 18:09:49 -0700

    ✨ Created logout and refresh routes

--- server.js
+++ server.js
@@ -3,14 +3,22 @@ const app = express()
 const path = require('path')
 const cors = require('cors')
 const corsOption = require('./Configs/corsOptions')
-const { logger } = require('./middleware/logEvents')
-const errorHandler = require('./middleware/errorHandler')
+const { logger } = require('./Middleware/logEvents')
+const errorHandler = require('./Middleware/errorHandler')
+const verifyJWT = require('./Middleware/verifyJWT')
+const credentials = require('./Middleware/credentials')
+const cookieParser = require('cookie-parser')
 const root = require('./Routes/root')
 const register = require('./Routes/register')
 const auth = require('./Routes/auth')
+const refreshToken = require('./Routes/refreshToken')
+const logout = require('./Routes/logout')
 const PORT = process.env.PORT || 3500
 
 // app.use(logger)
+
+// Handle options credentials check before CORS and fetch cookies credentials requirement
+app.use(credentials)
 
 // Cors middleware
 app.use(cors(corsOption))
@@ -19,6 +27,9 @@ app.use(cors(corsOption))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+// Cookie middleware
+app.use(cookieParser())
+
 // Static files
 app.use('/', express.static(path.join(__dirname, '/Public')))
 
@@ -26,6 +37,11 @@ app.use('/', express.static(path.join(__dirname, '/Public')))
 app.use('/', root)
 app.use('/register', register)
 app.use('/auth', auth)
+app.use('/refresh', refreshToken)
+app.use('/logout', logout)
+
+// Verify JWT
+app.use(verifyJWT)
 app.use('/employees', require('./Routes/api/employees'))
 
 app.all('*', (req, res) => {
