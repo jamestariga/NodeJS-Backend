4443788de2713cfb7969e3e1c56a730cc1508789
James <jamestariga08@live.com>
2022-05-25 18:09:11 -0700

    ✨ Implemented a middleware that will verify the JWT and credentials for CORS

---
+++ Middleware/verifyJWT.js
@@ -0,0 +1,17 @@
+const JWT = require('jsonwebtoken')
+require('dotenv').config()
+
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers['authorization']
+
+  if (!authHeader) return res.sendStatus(401)
+  console.log(authHeader)
+  const token = authHeader.split(' ')[1]
+  JWT.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) return res.sendStatus(403) // INVALID TOKEN
+    req.user = decoded.username
+    next()
+  })
+}
+
+module.exports = verifyJWT
