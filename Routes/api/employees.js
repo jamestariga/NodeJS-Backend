3d3be269e11e850ff39cb1f6ca743bec1126c237
James <jamestariga08@live.com>
2022-05-28 17:18:09 -0700

    ✨ Applied the middleware that will verify the user roles

--- Routes/api/employees.js
+++ Routes/api/employees.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const employeesController = require('../../Controllers/employeesController')
+const ROLES_LIST = require('../../Configs/roles_list')
+const verifyRoles = require('../../Middleware/verifyRoles')
 
 const {
   getAllEmployees,
@@ -13,9 +15,9 @@ const {
 router
   .route('/')
   .get(getAllEmployees)
-  .post(createEmployee)
-  .put(updateEmployee)
-  .delete(deleteEmployee)
+  .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), createEmployee)
+  .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), updateEmployee)
+  .delete(verifyRoles(ROLES_LIST.Admin), deleteEmployee)
 
 router.route('/:id').get(getEmployee)
 
