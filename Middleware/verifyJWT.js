10935f57f51a9946ccad9ed0306c560f52ce9aca
James <jamestariga08@live.com>
2022-05-28 17:17:26 -0700

    ✨ Added a middle that will verify the user roles

--- Middleware/verifyJWT.js
+++ Middleware/verifyJWT.js
@@ -2,14 +2,14 @@ const JWT = require('jsonwebtoken')
 require('dotenv').config()
 
 const verifyJWT = (req, res, next) => {
-  const authHeader = req.headers['authorization']
+  const authHeader = req.headers.authorization || req.headers.Authorization
 
-  if (!authHeader) return res.sendStatus(401)
-  console.log(authHeader)
+  if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401)
   const token = authHeader.split(' ')[1]
   JWT.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) return res.sendStatus(403) // INVALID TOKEN
-    req.user = decoded.username
+    req.user = decoded.UserInfo.username
+    req.roles = decoded.UserInfo.roles
     next()
   })
 }
